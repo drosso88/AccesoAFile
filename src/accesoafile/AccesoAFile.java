0fd2200490eac35eff13b4ef6358fbc170cec504
rocio <rocio.soriano.gomez@gmail.com>
2020-10-02 17:03:42 +0200

    con un metodo chuli que nos da la ultima modificacion del archivo de datos

--- src/accesoafile/AccesoAFile.java
+++ src/accesoafile/AccesoAFile.java
@@ -6,6 +6,7 @@
 package accesoafile;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 
 /**
  *
@@ -19,9 +20,11 @@ public class AccesoAFile {
     public static void main(String[] args) {
         //esto es la ruta relativa, en la que la carpeta debe de estar dentro del archivo del programa
         File f = new File("proyecto\\codigo_descarga_certificado.txt");
+        SimpleDateFormat sdf= new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
         if (f.exists()) {
             System.out.println("Nombre" + f.getName());
+            System.out.println("ultima modificación" + sdf.format(f.lastModified()));
             System.out.println("Directorio padre" + f.getParent());
             System.out.println("Ruta Relativa" + f.getPath());
             System.out.println("Ruta Absoluta" + f.getAbsolutePath());
