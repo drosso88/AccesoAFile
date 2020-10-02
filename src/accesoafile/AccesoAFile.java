50977fe1b591bb2870a329b34cc94b90b451e648
rocio <rocio.soriano.gomez@gmail.com>
2020-10-02 16:51:35 +0200

    ya hemos conseguido acceder

---
+++ src/accesoafile/AccesoAFile.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoafile;
+
+import java.io.File;
+
+/**
+ *
+ * @author rocio
+ */
+public class AccesoAFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //esto es la ruta relativa, en la que la carpeta debe de estar dentro del archivo del programa
+        File f = new File("proyecto\\codigo_descarga_certificado.txt");
+
+        if (f.exists()) {
+            System.out.println("Nombre" + f.getName());
+            System.out.println("Directorio padre" + f.getParent());
+            System.out.println("Ruta Relativa" + f.getPath());
+            System.out.println("Ruta Absoluta" + f.getAbsolutePath());
+        }else{
+            System.out.println("Aquí no hay nada oiga!");
+                    
+        }
+    }
+}
